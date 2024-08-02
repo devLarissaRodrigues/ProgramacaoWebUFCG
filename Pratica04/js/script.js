b1ab7af902b0f4311da3d3fee97c136e9cbba175
devLarissaRodrigues <larissa.rodrigues@ccc.ufcg.edu.br>
2024-08-02 15:20:36 -0300

    Adding initial questions

--- Pratica04/js/script.js
+++ Pratica04/js/script.js
@@ -1,5 +1,5 @@
 function calculaQuadrados(){
-    numeros = document.getElementById("numerosDigitados").value;
+    var numeros = document.getElementById("numerosDigitados").value;
     numeros = numeros.split(",");
 
     //document.write("Parabéns por usar nosso programa ultra moderno!" + "<br>"+"<br>");
@@ -12,7 +12,7 @@ function calculaQuadrados(){
 
 
 function verificaMaior(){
-    numeros = document.getElementById("numerosDigitados").value;
+    var numeros = document.getElementById("numerosDigitados").value;
     numeros = numeros.split(",");
     maior = parseInt(numeros[0]);
     for(i=1; i<numeros.length; i++){
@@ -21,4 +21,52 @@ function verificaMaior(){
         }
     }
     alert("O maior valor digitado é " + maior);
+}
+
+function verificaMaiorIdade(){
+    var idades = document.getElementById("idadesDigitadas").value;
+    idades = idades.split(",");
+    var maioridade = 0;
+    var menoridade = 0;
+
+    for(i=0; i<idades.length; i++){
+        if(parseInt(idades[i]) >= 18){
+            maioridade++;
+        }
+        else{
+            menoridade++;
+        }
+        mensagem = ("Há " + maioridade + " pessoa(s) de maior, enquanto há " + menoridade + " pessoa(s) de menor");
+    }
+    alert(mensagem);
+}
+
+function analisaData(){
+    var data = document.getElementById("dataDigitada").value;
+    data = data.split("/");
+    
+    alert("O dia é " + data[0] + ". O mês é " + data[1] + ". O ano é " + data[2]);
+}
+
+function inversorDeTexto(){
+    var texto = document.getElementById("textoDigitado").value;
+    var aux = '';
+
+    for(i=texto.length-1; i>=0; i--){
+        aux += texto[i];
+    }
+    alert("A inversão de '" + texto + "' é '" + aux + "'.");
+}
+
+function ordenaLista(){
+    var numeros = document.getElementById("numerosDigitados").value;
+    numeros = numeros.split(",");
+
+    let menor = numeros[0];
+    for(i=1; i<numeros.length; i++){
+        if(numeros[i] < menor){
+            menor = numeros[i];
+        }
+    }
+    alert(menor);
 }
